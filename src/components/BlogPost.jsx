eb1b72bf7867a77f26468b14120cfdc9f8925bf5
marco <enidev911@gmail.com>
2025-06-18 16:49:40 -0400

    clean: files and content

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -1,10 +1,8 @@
-import '../assets/post.css';
 
 export function BlogPost({ post }) {
   const { Content, title, date} = post;
   return (
-    <article className='post-container'>
-      <h1>{title}</h1>
+    <article>
       <time style={{ color: '#666', fontSize: '0.9rem' }}>{date}</time>
       <section style={{ marginTop: '2rem' }}>
           <Content />
