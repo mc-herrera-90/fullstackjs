96fba0bc3713cc9fe58184f5b4e21651e05af212
marco <enidev911@gmail.com>
2025-06-18 12:17:33 -0400

    up: BrowserRouter to HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx';
 import { ThemeProvider } from './context/ThemeContext.jsx';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
   <StrictMode>
    <ThemeProvider>
     <App />
   </ThemeProvider> 
   </StrictMode>
-  </BrowserRouter>
+  </HashRouter>
 )
--- vite.config.js
+++ vite.config.js
@@ -14,5 +14,6 @@ export default defineConfig({
       remarkPlugins: [remarkDirective, remarkCustomDirective, remarkGfm],
       rehypePlugins: [rehypeHighlight]
     })
-  ]
+  ],
+  base: '/'
 })
