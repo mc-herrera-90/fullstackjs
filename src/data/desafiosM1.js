bdb275eaf6dd7941c6b935c15f480b8b56a491b4
marco <enidev911@gmail.com>
2025-06-19 16:59:12 -0400

    up: content

--- src/data/desafiosM1.js
+++ src/data/desafiosM1.js
@@ -1,9 +1,11 @@
 import Desafio1 from '../pages/desafios/m1/calculadoraIMC.mdx';
 import Desafio2 from '../pages/desafios/m1/cachipun.mdx';
-import Desafio3 from '../pages/desafios/m1/factorial.mdx';
+import Desafio3 from '../pages/desafios/m1/fibonacci.mdx';
+import Desafio4 from '../pages/desafios/m1/factorial.mdx';
 
 export const desafiosM1 = [
     {title: 'Calculadora IMC', difficult: 'Fácil', slug: 'calculadora-imc', component: Desafio1},
     {title: 'Cachipún', difficult: 'Fácil', slug: 'cachipun', component: Desafio2},
-    {title: 'Factorial', difficult: 'Medio', slug: 'factorial', component: Desafio3}
+    {title: 'Fibonacci', difficult: 'Fácil', slug: 'factorial', component: Desafio3},
+    {title: 'Factorial', difficult: 'Medio', slug: 'fibonacci', component: Desafio4}
 ]
