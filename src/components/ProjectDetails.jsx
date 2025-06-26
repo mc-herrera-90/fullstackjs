20c6e73a328cc73859c264bb966226a1109d82e0
marco <enidev911@gmail.com>
2025-06-26 19:12:18 -0400

    up: content

---
+++ src/components/ProjectDetails.jsx
@@ -0,0 +1,48 @@
+import ReactMarkdown from "react-markdown";
+
+import "./ProjectDetails.css";
+
+const ProjectDetails = ({ details }) => {
+  return (
+    <div className="project-container">
+      <h2 className="project-title">{details.name}</h2>
+      <p className="project-abstract">
+        <ReactMarkdown>{details.abstract}</ReactMarkdown>
+      </p>
+      <table className="project-table">
+        <tbody>
+          <tr>
+            <th>Lenguaje usado:</th>
+            <td>{details.languages}</td>
+          </tr>
+          <tr>
+            <th>IDE:</th>
+            <td>{details.ide}</td>
+          </tr>
+          <tr>
+            <th>Java Version:</th>
+            <td>{details.javaVersion}</td>
+          </tr>
+          <tr>
+            <th>Base de Datos:</th>
+            <td>{details.database ? details.database : 'No'}</td>
+          </tr>
+          <tr>
+            <th>Tipo:</th>
+            <td>{details.type}</td>
+          </tr>
+          <tr>
+            <th>Recomendado para:</th>
+            <td>{details.recommended}</td>
+          </tr>
+          <tr>
+            <th>Tiempo de desarrollo:</th>
+            <td>{details.time}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ProjectDetails;
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.jsx';
 import { ThemeProvider } from './context/ThemeContext.jsx';
 import { HashRouter } from "react-router-dom";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 createRoot(document.getElementById('root')).render(
   <HashRouter>
